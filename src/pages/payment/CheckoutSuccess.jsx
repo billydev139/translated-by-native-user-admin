30eae120470eb9046a15f900d51229315489b019
MuhammadUsama910 <muhammadusama44910@gmail.com>
2024-10-01 10:44:06 +0500

    Some Changes in Checkout

--- src/pages/payment/CheckoutSuccess.jsx
+++ src/pages/payment/CheckoutSuccess.jsx
@@ -9,8 +9,8 @@ function CheckoutSuccess() {
     //   <button onClick={() => window.location.href = '/'}>Go back to Homepage</button>
     // </div>
 
-    <div class="bg-gray-100 h-screen">
-      <div class="bg-white p-6  md:mx-auto">
+    <div class="bg-white flex justify-center items-center h-screen">
+      <div class="bg-white p-6 md:mx-auto">
         <svg viewBox="0 0 24 24" class="text-green-600 w-16 h-16 mx-auto my-6">
             <path fill="currentColor"
                 d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z">
